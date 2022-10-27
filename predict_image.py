56efe735938a443a99feb2ceb53ebb5567351ef9
Vikasguleria7 <96754852+Vikasguleria7@users.noreply.github.com>
2022-10-27 00:24:37 -0600

    Add files via upload

---
+++ predict_image.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 26 22:07:40 2022
+
+@author: vikas
+"""
+
+## Predicitng Models
+
+
+from keras.models import load_model
+from keras.preprocessing import image
+from keras.applications.vgg16 import preprocess_input
+import numpy as np
+model = load_model('model_vgg16.h5')
+img = image.load_img('val/PNEUMONIA/person1946_bacteria_1132.jpeg', target_size=(224, 224))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+img_data = preprocess_input(x)
+classes = model.predict(img_data)
